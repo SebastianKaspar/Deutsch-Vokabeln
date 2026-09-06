a813a31eec26bf846ab15ee3802f11436559d8d6
SebastianKaspar <sebastian.a.kaspar@gmail.com>
2026-09-06 18:42:08 +0200

    1036 Vokabeln, Enter-Taste, PWA-Dateien

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-﻿const CACHE = "vokabeln-v2";
+const CACHE = "vokabeln-v3";
 const DATEIEN = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
+
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(DATEIEN)).then(() => self.skipWaiting()));
 });
@@ -8,5 +9,16 @@ self.addEventListener("activate", e => {
     Promise.all(k.filter(n => n !== CACHE).map(n => caches.delete(n)))).then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(t => t || fetch(e.request)));
+  const istSeite = e.request.mode === "navigate" || e.request.destination === "document";
+  if (istSeite) {
+    e.respondWith(
+      fetch(e.request).then(antwort => {
+        const kopie = antwort.clone();
+        caches.open(CACHE).then(c => c.put("./index.html", kopie));
+        return antwort;
+      }).catch(() => caches.match("./index.html"))
+    );
+  } else {
+    e.respondWith(caches.match(e.request).then(t => t || fetch(e.request)));
+  }
 });
